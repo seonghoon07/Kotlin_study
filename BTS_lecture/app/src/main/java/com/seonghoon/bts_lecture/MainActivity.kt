781902bc332ef5068220a4a690b3595882dd2732
seonghoon07 <chltjdgns1009@gmail.com>
2025-01-26 12:36:36 +0900

    setOnClickListener

--- BTS_lecture/app/src/main/java/com/seonghoon/bts_lecture/MainActivity.kt
+++ BTS_lecture/app/src/main/java/com/seonghoon/bts_lecture/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.seonghoon.bts_lecture
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 화면이 클릭되었음을 알리기
+        val image1 = findViewById<ImageView>(R.id.btsImage1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+        }
     }
 }
