3dc5185552e5476771df3c1e7324e359b90eaa69
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-01 19:39:02 +0900

    Twice intent putExtra

--- Twice/app/src/main/java/com/seonghoon/twice/MainActivity.kt
+++ Twice/app/src/main/java/com/seonghoon/twice/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.seonghoon.twice
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val btn1 = findViewById<ImageView>(R.id.member1)
+        val btn2 = findViewById<ImageView>(R.id.member2)
+        val btn3 = findViewById<ImageView>(R.id.member3)
+        val btn4 = findViewById<ImageView>(R.id.member4)
+        val btn5 = findViewById<ImageView>(R.id.member5)
+        val btn6 = findViewById<ImageView>(R.id.member6)
+        val btn7 = findViewById<ImageView>(R.id.member7)
+        val btn8 = findViewById<ImageView>(R.id.member8)
+        val btn9 = findViewById<ImageView>(R.id.member9)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "1")
+            startActivity(intent)
         }
+        btn2.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "2")
+            startActivity(intent)
+        }
+        btn3.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "3")
+            startActivity(intent)
+        }
+        btn4.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "4")
+            startActivity(intent)
+        }
+        btn5.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "5")
+            startActivity(intent)
+        }
+        btn6.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "6")
+            startActivity(intent)
+        }
+        btn7.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "7")
+            startActivity(intent)
+        }
+        btn8.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "8")
+            startActivity(intent)
+        }
+        btn9.setOnClickListener {
+            val intent = Intent(this, ImageInsideActivity::class.java)
+            intent.putExtra("data", "9")
+            startActivity(intent)
+        }
+
     }
 }
