679c2f9dc883659a546036da1175f816a4aa4504
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-02 23:38:07 +0900

    dataBinding Databinding 형식

--- databinding_ex/app/src/main/java/com/seonghoon/databinding_ex/MainActivity.kt
+++ databinding_ex/app/src/main/java/com/seonghoon/databinding_ex/MainActivity.kt
@@ -3,20 +3,28 @@ package com.seonghoon.databinding_ex
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.seonghoon.databinding_ex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         // 1번 방식 - findViewById
-        val btn = findViewById<Button>(R.id.testBtnId)
-        btn.setOnClickListener {
-            Toast.makeText(this, "click", Toast.LENGTH_LONG ).show()
+//        val btn = findViewById<Button>(R.id.testBtnId)
+//        btn.setOnClickListener {
+//            Toast.makeText(this, "click", Toast.LENGTH_LONG ).show()
+//        }
+
+        // 2번 방식 - DataBinding
+        binding.testBtnId.setOnClickListener {
+            Toast.makeText(this, "click", Toast.LENGTH_SHORT).show()
         }
     }
 }
