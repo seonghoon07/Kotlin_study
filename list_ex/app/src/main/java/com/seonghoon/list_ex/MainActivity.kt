b36c35faf61b1e1704d4ba12e3a724a88bfb9f39
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-09 23:28:12 +0900

    list_ex Kotlin list

---
+++ list_ex/app/src/main/java/com/seonghoon/list_ex/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.seonghoon.list_ex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+    }
+}
