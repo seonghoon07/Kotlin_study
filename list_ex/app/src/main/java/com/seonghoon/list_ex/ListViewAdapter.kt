b7917aae5e0e2a61a49f1f7a04e52cf4aa6d863f
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-10 23:53:00 +0900

    listview_ex adapter 추가

---
+++ list_ex/app/src/main/java/com/seonghoon/list_ex/ListViewAdapter.kt
@@ -0,0 +1,30 @@
+package com.seonghoon.list_ex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListViewAdapter(val List : MutableList<String>): BaseAdapter() {
+    override fun getCount(): Int {
+        return List.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return List[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent?.context).inflate(R.layout.listview_item, parent, false)
+        }
+
+        return convertView!!
+    }
+}
