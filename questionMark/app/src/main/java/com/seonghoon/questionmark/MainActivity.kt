535f5aeda251a76927bb990af3f2a1b312d8abbb
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-11 15:18:17 +0900

    questionMark 코틀린 ?, ;

---
+++ questionMark/app/src/main/java/com/seonghoon/questionmark/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.seonghoon.questionmark
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val value: String? = "a"
+        val value2: String? = null
+        
+        // ? -> null 일 수 있음
+        // ! -> null 이 아님
+
+    }
+}
