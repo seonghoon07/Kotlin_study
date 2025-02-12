948c2c9456b92042624b84f0c543a47780ec773e
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-12 15:39:30 +0900

    GoodWords 명언리스트 페이지 추가

--- GoodWords/app/src/main/java/com/seonghoon/goodwords/SentenceActivity.kt
+++ GoodWords/app/src/main/java/com/seonghoon/goodwords/SentenceActivity.kt
@@ -1,6 +1,7 @@
 package com.seonghoon.goodwords
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class SentenceActivity: AppCompatActivity() {
@@ -8,6 +9,20 @@ class SentenceActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sentence)
 
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("명언1")
+        sentenceList.add("명언2")
+        sentenceList.add("명언3")
+        sentenceList.add("명언4")
+        sentenceList.add("명언5")
+        sentenceList.add("명언6")
+        sentenceList.add("명언7")
+        sentenceList.add("명언8")
+
+        val sentenceAdapter = ListViewAdapter(sentenceList)
+        val listView = findViewById<ListView>(R.id.sentenceView)
+
+        listView.adapter = sentenceAdapter
 
     }
 }
