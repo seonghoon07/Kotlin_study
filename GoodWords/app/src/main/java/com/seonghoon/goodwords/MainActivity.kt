dc93f0a189e1f29f5ff3e876a8174ee9b8fc0b1d
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-12 15:11:48 +0900

    GoodWords 레이아웃 설정

--- GoodWords/app/src/main/java/com/seonghoon/goodwords/MainActivity.kt
+++ GoodWords/app/src/main/java/com/seonghoon/goodwords/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.seonghoon.goodwords
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.seonghoon.goodwords.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.showAllSentenceBtn.setOnClickListener {
+            val intent = Intent(this, SentenceActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ GoodWords/app/src/main/java/com/seonghoon/goodwords/SentenceActivity.kt
@@ -0,0 +1,13 @@
+package com.seonghoon.goodwords
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SentenceActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentence)
+
+
+    }
+}
