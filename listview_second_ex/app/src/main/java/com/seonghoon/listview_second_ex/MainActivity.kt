c73d75ace1cc5c56114177aeae13d42549964484
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-17 16:25:49 +0900

    커밋 테스트

--- listview_second_ex/app/src/main/java/com/seonghoon/listview_second_ex/MainActivity.kt
+++ listview_second_ex/app/src/main/java/com/seonghoon/listview_second_ex/MainActivity.kt
@@ -3,10 +3,7 @@ package com.seonghoon.listview_second_ex
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
