1e2075e7e6ed56159810bbfbaba95e960ef55040
최성훈 <seonghoon07@choeseonghun-ui-MacBookAir.local>
2025-02-17 16:15:01 +0900

    listview_second_ex 리스트뷰 복습 및 아이템 클릭 이벤트처리

---
+++ listview_second_ex/app/src/main/java/com/seonghoon/listview_second_ex/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.seonghoon.listview_second_ex
+
+data class ListViewModel (
+    val text1: String,
+    val text2: String
+)
---
+++ listview_second_ex/app/src/main/java/com/seonghoon/listview_second_ex/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.seonghoon.listview_second_ex
+
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val list_item = mutableListOf<String>()
+//
+//        list_item.add("A")
+//        list_item.add("B")
+//        list_item.add("C")
+
+        val list_item2 = mutableListOf<ListViewModel>()
+        list_item2.add(ListViewModel("a", "b"))
+        list_item2.add(ListViewModel("c", "d"))
+        list_item2.add(ListViewModel("e", "f"))
+
+        val listviewAdapter = ListViewAdapter(list_item2)
+
+        val listview = findViewById<ListView>(R.id.mainListView)
+        listview.adapter = listviewAdapter
+
+        listview.setOnItemClickListener { parent, view, position, id ->
+
+            Toast.makeText(this, list_item2[position].text1, Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
