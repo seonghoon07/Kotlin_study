e5c6be51544568b415f86cad412957d2bfd2efb3
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-24 14:51:07 +0900

    trot_list recyclerView 생성

--- trot_list/app/src/main/java/com/seonghoon/trot_list/Singer2Fragment.kt
+++ trot_list/app/src/main/java/com/seonghoon/trot_list/Singer2Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Singer2Fragment : Fragment() {
 
@@ -19,6 +21,31 @@ class Singer2Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_singer2, container, false)
+
+        val items = mutableListOf<String>()
+        items.add("별빛같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+
+
+        val rv = view.findViewById<RecyclerView>(R.id.singRV)
+        val rvAdapter = RVAdapter(items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
 
         view.findViewById<ImageView>(R.id.image1).setOnClickListener {
             it.findNavController().navigate(R.id.action_singer2Fragment_to_singer1Fragment)
--- trot_list/app/src/main/java/com/seonghoon/trot_list/Singer3Fragment.kt
+++ trot_list/app/src/main/java/com/seonghoon/trot_list/Singer3Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Singer3Fragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,30 @@ class Singer3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_singer3, container, false)
+
+        val items = mutableListOf<String>()
+        items.add("피어나")
+        items.add("진실 혹은 대답")
+        items.add("우리 사랑하게 됐어요")
+        items.add("피어나")
+        items.add("진실 혹은 대답")
+        items.add("우리 사랑하게 됐어요")
+        items.add("피어나")
+        items.add("진실 혹은 대답")
+        items.add("우리 사랑하게 됐어요")
+        items.add("피어나")
+        items.add("진실 혹은 대답")
+        items.add("우리 사랑하게 됐어요")
+        items.add("피어나")
+        items.add("진실 혹은 대답")
+        items.add("우리 사랑하게 됐어요")
+
+
+        val rv = view.findViewById<RecyclerView>(R.id.singRV)
+        val rvAdapter = RVAdapter(items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
 
         view.findViewById<ImageView>(R.id.image1).setOnClickListener {
             it.findNavController().navigate(R.id.action_singer3Fragment_to_singer1Fragment)
