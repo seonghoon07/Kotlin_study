755f5ba0b0bb3df163456edacfe7f5b55c623264
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-24 14:17:43 +0900

    trot_list navigate 설정

---
+++ trot_list/app/src/main/java/com/seonghoon/trot_list/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.seonghoon.trot_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 1. navigation 만들기
+
+        // 2. Fragment 만들고 연결
+
+        // 3. recyclerView 만들기
+
+    }
+}
---
+++ trot_list/app/src/main/java/com/seonghoon/trot_list/Singer2Fragment.kt
@@ -0,0 +1,33 @@
+package com.seonghoon.trot_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
+
+class Singer2Fragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_singer2, container, false)
+
+        view.findViewById<ImageView>(R.id.image1).setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer2Fragment_to_singer1Fragment)
+        }
+
+        view.findViewById<ImageView>(R.id.image3).setOnClickListener {
+            it.findNavController().navigate(R.id.action_singer2Fragment_to_singer3Fragment)
+        }
+
+        return view
+    }
+}
