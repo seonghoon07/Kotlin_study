f026aeaf91ee8d358db09a280e567c52cd2b9419
seonghoon07 <chltjdgns1009@gmail.com>
2025-02-24 22:18:14 +0900

    diet_memo 다이얼로그 구현

--- diet_memo/app/src/main/java/com/seonghoon/diet_memo/MainActivity.kt
+++ diet_memo/app/src/main/java/com/seonghoon/diet_memo/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.seonghoon.diet_memo
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.icu.util.GregorianCalendar
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,7 +20,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val writeBtn = findViewById<ImageView>(R.id.writeBtn)
+        writeBtn.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
 
+            val mAlertDialog = mBuilder.show()
+
+            val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+            dateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year: Int = today.get(Calendar.YEAR)
+                val month: Int = today.get(Calendar.MONTH)
+                val date: Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                        Log.d("main", "${year}, ${month+1}, ${date}")
+                        dateSelectBtn.setText("${year}, ${month+1}, ${date}")
+                    }
+
+                }, year, month, date)
+                dlg.show()
+            }
+
+        }
 
     }
 }
